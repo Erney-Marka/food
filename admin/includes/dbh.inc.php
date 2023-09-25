2e9afb656ebb51b4308ceb5155eb7a532528bf0a
Erney-Marka <erneybal@gmail.com>
2023-09-25 11:34:32 +0600

    commit 0.3

--- admin/includes/dbh.inc.php
+++ admin/includes/dbh.inc.php
@@ -1,2 +1,12 @@
 <?php
 
+$serverName = 'localhost';
+$dbUserName = 'root';
+$dbPass = '';
+$dbName = 'fooddb';
+
+$conn = mysqli_connect($serverName, $dbUserName, $dbPass, $dbName);
+
+if (!$conn) {
+    die('Connection failed: ' . mysqli_connect_error());
+}
--- admin/includes/signup.inc.php
+++ admin/includes/signup.inc.php
@@ -1,11 +1,29 @@
 <?php
+require_once 'dbh.inc.php';
 
 // если кнопка нажата
 if (isset($_POST['submit'])) {
 
-// Получить данные из формы
-$fullName = $_POST['full_name'];
-$username = $_POST['username'];
-$pwd = $_POST['pwd'];
-    
+    // Получить данные из формы
+    $fullName = $_POST['full_name'];
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];
+
+    // сохранение в базе
+    $sql = "INSERT INTO tbl_admin (full_name, username, pwd) VALUES (?, ?, ?);";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header('Location: ../admin/add_admin.php?error=stmtfailed');
+        exit();
+    }
+
+    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+    mysqli_stmt_bind_param($stmt, 'sss', $fullName, $username, $hashedPwd);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    header('Location: ../admin/add_admin.php?error=none');
+    exit();
 }
